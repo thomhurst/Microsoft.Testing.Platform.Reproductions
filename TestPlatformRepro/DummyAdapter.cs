9062a428f38c7f842fafa52c560e2574d783e6aa
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-03-03 10:51:49 +0000

    Add repro solution

---
+++ TestPlatformRepro/DummyAdapter.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Testing.Platform.Extensions.Messages;
+using Microsoft.Testing.Platform.Extensions.TestFramework;
+using Microsoft.Testing.Platform.TestHost;
+
+internal class DummyAdapter : ITestFramework, IDataProducer
+{
+    public string Uid => nameof(DummyAdapter);
+
+    public string Version => string.Empty;
+
+    public string DisplayName => string.Empty;
+
+    public string Description => string.Empty;
+
+    public Type[] DataTypesProduced => [typeof(TestNodeUpdateMessage)];
+
+    public Task<CloseTestSessionResult> CloseTestSessionAsync(CloseTestSessionContext context) => Task.FromResult(new CloseTestSessionResult { IsSuccess = true });
+
+    public Task<CreateTestSessionResult> CreateTestSessionAsync(CreateTestSessionContext context) => Task.FromResult(new CreateTestSessionResult { IsSuccess = true });
+
+    public async Task ExecuteRequestAsync(ExecuteRequestContext context)
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            await context.MessageBus.PublishAsync(this, new TestNodeUpdateMessage(new SessionUid("1"), new TestNode
+            {
+                Uid = $"{i}",
+                DisplayName = $"Test {i}",
+                Properties = new PropertyBag(InProgressTestNodeStateProperty.CachedInstance)
+            }));
+        }
+
+        await Task.Delay(TimeSpan.FromSeconds(30));
+     
+        context.Complete();
+    }
+
+    public Task<bool> IsEnabledAsync() => Task.FromResult(true);
+}
---
+++ TestPlatformRepro/TestingPlatformBuilderHook.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Testing.Platform.Builder;
+using Microsoft.Testing.Platform.Capabilities.TestFramework;
+
+namespace TestPlatformRepro;
+
+public class TestingPlatformBuilderHook
+{
+    public static void AddExtensions(
+        ITestApplicationBuilder builder,
+        string[] _)
+    {
+        var testFramework = new DummyAdapter();
+
+        builder.RegisterTestFramework(_ => new TestFrameworkCapabilities(), (_, _) => testFramework);
+    }
+}
